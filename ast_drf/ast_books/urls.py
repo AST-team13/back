da325b287a5a935d27c2b36d0bfd48802aba24f4
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-12-19 18:37:07 +0300

    отредачл эндпоинты

--- ast_drf/ast_books/urls.py
+++ ast_drf/ast_books/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 
-urlpatterns = format_suffix_patterns([
+urlpatterns = [
     path('reviews/', ReviewViewSet.as_view({'get': 'list'})),
-    path('reviews/<int:pk>/', ReviewViewSet.as_view({'get': 'retrieve'})),
     path('reviews-picture/', ReviewPictureViewSet.as_view({'get': 'list'})),
-    path('reviews-picture/<int:pk>/', ReviewPictureViewSet.as_view({'get': 'retrieve'})),
     path('books/', BookViewSet.as_view({'get': 'list'})),
-    path('books/<int:pk>/', BookViewSet.as_view({'get': 'retrieve'})),
-    path('books/<int:pk>/picture/', BookPictureViewSet.as_view({'get': 'list'})),
-    path('books/<int:pk>/picture/<int:picture_pk>/', BookPictureViewSet.as_view({'get': 'retrieve'})),
+    path('books-picture/', BookPictureViewSet.as_view({'get': 'list'})),
     path('audio/', AudioFileViewSet.as_view({'get': 'list'})),
-    path('audio/<int:pk>/', AudioFileViewSet.as_view({'get': 'retrieve'})),
-])
+]
