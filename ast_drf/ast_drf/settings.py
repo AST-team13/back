61eba4c93866dd39a1c12b1b3a51577c4f194654
Odiosus <barbell.wire0b@icloud.com>
2024-12-20 14:12:10 +0300

    set MEDIA_ROOT

--- ast_drf/ast_drf/settings.py
+++ ast_drf/ast_drf/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/opt/render/project/src/ast_drf/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
