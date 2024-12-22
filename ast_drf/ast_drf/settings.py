59722d9fcf01485ab3c8cf08e451c7ff180be69b
Odiosus <barbell.wire0b@icloud.com>
2024-12-22 18:20:58 +0300

    добавлены настройки корс

--- ast_drf/ast_drf/settings.py
+++ ast_drf/ast_drf/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'django_crontab',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # доп. указано через консоль PAW
 ]
 
 ROOT_URLCONF = 'ast_drf.urls'
