091a3479542926f59e3a08c208a1c9ed38f8f363
Odiosus <barbell.wire0b@icloud.com>
2024-12-22 21:05:36 +0300

    корректировка парсера

--- ast_drf/ast_books/services.py
+++ ast_drf/ast_books/services.py
@@ -27,7 +27,7 @@ class ReviewService:
                     create = item.get('createdDate')
 
                     # Фильтруем отзывы
-                    if rating == 5 and (len(text) >= 20 or len(pros) >= 20):
+                    if rating == 5 and (len(text) >= 60 or len(pros) >= 60):
                         reviews.append({
                             'author': author,
                             'create': create,
