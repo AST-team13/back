cb47535e12e7be2835c5ba9d7e9df8c888e68f8b
Odiosus <barbell.wire0b@icloud.com>
2024-12-23 10:36:32 +0300

    корректировка парсера (ограничения 60-240)

--- ast_drf/ast_books/services.py
+++ ast_drf/ast_books/services.py
@@ -27,7 +27,7 @@ class ReviewService:
                     create = item.get('createdDate')
 
                     # Фильтруем отзывы
-                    if rating == 5 and 20 >= len(text) <= 240 or 20 >= len(pros) <= 240:
+                    if rating == 5 and 60 >= len(text) <= 240 or 60 >= len(pros) <= 240:
                         reviews.append({
                             'author': author,
                             'create': create,
